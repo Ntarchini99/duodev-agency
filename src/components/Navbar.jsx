cb5c69bad5d86236c72ddacb82b091ca376c829f
Nazareno Tarchini <128329403+Ntarchini99@users.noreply.github.com>
2023-11-13 07:50:27 +0000

    cambios en el responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
-      setShowMenu(false); 
+      setShowMenu(false);
     }
   };
 
@@ -17,13 +17,15 @@ const Header = () => {
       <div className="xl:w-1/6 text-center -mt-4">
         <a href="#" className="text-2xl font-bold relative p-1">DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' /></a>
       </div>
-      <nav className={`bg-white w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}>
+      <div className="xl:w-full xl:hidden">
+        <button onClick={() => setShowMenu(!showMenu)} className='text-2xl p-2'>{showMenu ? <RiCloseLine /> : <RiMenu3Fill />}</button>
+      </div>
+      <nav className={`bg-white w-[80%] md:w-[40%] xl:w-full h-full xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50 ${showMenu ? "absolute top-16 left-0" : "hidden xl:flex"}`}>
         <button onClick={() => scrollToSection('Home')} className='font-bold' type="button">Inicio</button>
         <button onClick={() => scrollToSection('Works')} className='font-bold' type="button">Portfolio</button>
         <button onClick={() => scrollToSection('Services')} className='font-bold' type="button">Servicios</button>
         <button onClick={() => scrollToSection('Nosotros')} className='font-bold' type="button">Nosotros</button>
       </nav>
-      <button onClick={() => setShowMenu(!showMenu)} className='xl:hidden text-2xl p-2'>{showMenu ? <RiCloseLine /> : <RiMenu3Fill />}</button>
     </header>
   );
 };
