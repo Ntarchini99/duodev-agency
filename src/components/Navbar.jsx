29fa898254c5615295be2cd415ec082de7a5c99d
Nazareno Tarchini <128329403+Ntarchini99@users.noreply.github.com>
2023-11-13 07:11:05 +0000

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header className={`fixed top-0 w-full py-4 px-8 z-50 ${showMenu ? "xl:shadow-lg" : ""}`}>
       <div className="xl:w-1/6 text-center -mt-4">
-        <a href="#" className="text-2xl font-bold relative p-1 bg-white">DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' /></a>
+        <a href="#" className="text-2xl font-bold relative p-1">DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' /></a>
       </div>
       <nav className={`w-full xl:w-[80%] md:w-[40%] h-full ${showMenu ? "block" : "hidden"} xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}>
         <button onClick={() => scrollToSection('Home')} className='font-bold' type="button">Inicio</button>
