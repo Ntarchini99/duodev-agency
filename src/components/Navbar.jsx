2216009c6cc764fa10a5420add4a42b0ccb29cc8
Nazareno Tarchini <128329403+Ntarchini99@users.noreply.github.com>
2023-11-13 07:16:55 +0000

    xambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ const Header = () => {
   };
 
   return (
-    <header className="flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50">
+    <header className={`flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50 fixed top-0`}>
       <div className="xl:w-1/6 text-center -mt-4">
         <a href="#" className="text-2xl font-bold relative p-1 bg-white">DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' /></a>
       </div>
-      <nav className={`fixed bg-white w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}>
+      <nav className={`bg-white w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}>
         <button onClick={() => scrollToSection('Home')} className='font-bold' type="button">Inicio</button>
         <button onClick={() => scrollToSection('Works')} className='font-bold' type="button">Portfolio</button>
         <button onClick={() => scrollToSection('Services')} className='font-bold' type="button">Servicios</button>
