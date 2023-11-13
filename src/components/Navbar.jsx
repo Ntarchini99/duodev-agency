57e5f4ce215ea7f79a4113072cf2ffcc2f2dfc32
Nazareno Tarchini <128329403+Ntarchini99@users.noreply.github.com>
2023-11-13 07:45:24 +0000

    cambios en la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,58 +8,22 @@ const Header = () => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
-      setShowMenu(false);
+      setShowMenu(false); 
     }
   };
 
   return (
     <header className={`bg-white flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50 fixed top-0`}>
       <div className="xl:w-1/6 text-center -mt-4">
-        <a href="#" className="text-2xl font-bold relative p-1">
-          DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} 
-          <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' />
-        </a>
+        <a href="#" className="text-2xl font-bold relative p-1">DuoDev<span className='text-blue-500 text-5xl'>.</span>{" "} <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-blue-500 -z-10' /></a>
       </div>
-      <button
-        onClick={() => setShowMenu(!showMenu)}
-        className='xl:hidden text-2xl p-2 focus:outline-none'
-      >
-        {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
-      </button>
-      <nav
-        className={`bg-white w-full md:w-[80%] xl:w-full ${
-          showMenu ? "block" : "hidden"
-        } md:flex md:justify-end md:items-center h-screen md:h-auto transition-all duration-500`}
-      >
-        <button
-          onClick={() => scrollToSection('Home')}
-          className='font-bold py-2 md:py-0 md:px-4 md:ml-4 text-black'
-          type="button"
-        >
-          Inicio
-        </button>
-        <button
-          onClick={() => scrollToSection('Works')}
-          className='font-bold py-2 md:py-0 md:px-4 md:ml-4 text-black'
-          type="button"
-        >
-          Portfolio
-        </button>
-        <button
-          onClick={() => scrollToSection('Services')}
-          className='font-bold py-2 md:py-0 md:px-4 md:ml-4 text-black'
-          type="button"
-        >
-          Servicios
-        </button>
-        <button
-          onClick={() => scrollToSection('Nosotros')}
-          className='font-bold py-2 md:py-0 md:px-4 md:ml-4 text-black'
-          type="button"
-        >
-          Nosotros
-        </button>
+      <nav className={`bg-white w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}>
+        <button onClick={() => scrollToSection('Home')} className='font-bold' type="button">Inicio</button>
+        <button onClick={() => scrollToSection('Works')} className='font-bold' type="button">Portfolio</button>
+        <button onClick={() => scrollToSection('Services')} className='font-bold' type="button">Servicios</button>
+        <button onClick={() => scrollToSection('Nosotros')} className='font-bold' type="button">Nosotros</button>
       </nav>
+      <button onClick={() => setShowMenu(!showMenu)} className='xl:hidden text-2xl p-2'>{showMenu ? <RiCloseLine /> : <RiMenu3Fill />}</button>
     </header>
   );
 };
