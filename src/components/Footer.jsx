766363ecc555ea552ddca2c1dac060b12b943149
Nazareno Tarchini <128329403+Ntarchini99@users.noreply.github.com>
2023-11-14 07:38:42 +0000

    cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,13 @@ const Footer = () => {
     <footer className='bg-gray-700 p-4 xl:p-4'>
       <div className='flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 border-b border-gray-500 pb-4'>
         <nav className='flex items-center gap-4 md:w-1/2 justify-center md:justify-start'>
-          <a href='https://www.instagram.com/duodevs_/' target='blank' className='block text-white p-4 rounded-full'>
+          <a href='https://instagram.com/duodevagency?igshid=MzMyNGUyNmU2YQ==' target='blank' className='block text-white p-4 rounded-full'>
             {""}<i className="fa-brands fa-instagram"></i>{""}
           </a>
-          <a href='https://www.facebook.com/jonnadev89' target='blank' className='block text-white p-4 rounded-full'>
-            {""}<i className="fa-regular fa-envelope"></i>{""}
+          <a href='mailto:duodevagency@gmail.com' className='block text-white p-4 rounded-full'>
+               <i className="fa-regular fa-envelope"></i>
           </a>
+
         </nav>
         <div className='md:w-1/2 text-center md:text-left'>
         </div>
